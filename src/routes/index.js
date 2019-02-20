b5bacf86245ede5b003c775e3ee648c4cbb12797
Tada <thomas.bernard72@gmail.com>
2019-02-20 22:56:17 +0100

    Inc nb Object de l'user -> Reste à vérifier le type de l'objet (voir si prioritaire ou non)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var auth = require("../controllers/authController.js");
-var multer  = require('multer')
+var multer  = require('multer');
+var path = require('path');
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads')
@@ -10,7 +11,14 @@ var storage = multer.diskStorage({
     cb(null, file.fieldname + '-' + Date.now())
   }
 });
-var upload = multer({storage: storage});
+var upload = multer({storage: storage, 
+  fileFilter: function (req, file, cb) {
+    if (!file.originalname.match(/\.(fbx|obj)$/)) {
+        return cb(new Error('Only 3D object files are allowed!'));
+    }
+    cb(null, true);
+  }
+});
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var url = 'mongodb://localhost:27017/mixedcad360';
@@ -38,22 +46,29 @@ router.get('/myObjects', auth.myObjects);
 router.get('/uploadObject', auth.uploadObject);
 
 router.post('/processobjectupload', upload.single('file'), (req, res, next) => {
-  
+  if(err) res.render('upload_object', { user : req.user });
+
     MongoClient.connect(url, (err, db) => {
-        assert.equal(null, err);
-        insertDocuments(db, 'uploads/' + req.file.filename, req.user.username, () => {
-            db.close();
+        assert(null, err);
+
+        //Ajout du chemin de l'objet dans la BDD (+ autres infos)
+        db.collection('objects').insertOne(
+          {'imagePath' : 'uploads/' + req.file.filename, 
+          'username' : req.user.username,
+          'objectName': req.body.objectName }, 
+          (err, result) => {
+            if ( err ) throw err;
+        });
+
+        //Update de l'user (nombre d'objet)
+        db.collection( 'users' ).update (
+          { username : req.user.username },
+          { $inc : { objects:1 } },
+          function( err, result ) {
+              if ( err ) throw err;
         });
     });
     res.render('index', { user : req.user });
 });
-
-var insertDocuments = function(db, filePath, username, callback) {
-  var collection = db.collection('objects');
-  collection.insertOne({'imagePath' : filePath, 'username' : username }, (err, result) => {
-      assert.equal(err, null);
-      callback(result);
-  });
-}
 
 module.exports = router;
